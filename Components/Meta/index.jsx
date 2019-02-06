337095510463778568d1c78b421ce2a5bb3d17a9
Adam Mescher <admescher@gmail.com>
2019-02-05 22:31:22 -0700

    Add Meta component

---
+++ Components/Meta/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Meta = () => (
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charSet="utf-8" />
+    <meta name="author" content="Nate Mescher" />
+    <meta name="description" content="Nate Mescher portfolio for NVTEVISUALS freelance event photography and videography" />
+    <title>NVTEVISUALS</title>
+  </Head>
+);
+
+export default Meta;
