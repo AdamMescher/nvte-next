7b2a41bc04c6b21aa1e0ad49ad07314d1d44a1a6
Adam Mescher <admescher@gmail.com>
2019-02-07 16:21:28 -0700

    Add id so that nav link click redirects to correct section on page

--- Components/About/index.jsx
+++ Components/About/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import StyledAbout from './styled';
 
 const About = () => (
-    <StyledAbout>
+    <StyledAbout id="about">
         <h2>about</h2>
         <p>Demonstrating agile but surprise and delight. Inform mobile-first design in order to create synergy. Synchronise branding yet improve overall outcomes. Creating vertical integration and then surprise and delight. Informing dark social with the possibility to re-target key demographics.</p>
     </StyledAbout>
