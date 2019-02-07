9c9e0b0e38baa12593358942307fc6a7628ba752
Adam Mescher <admescher@gmail.com>
2019-02-06 17:05:58 -0700

    Add Logo and StyledLogo components

---
+++ Components/Logo/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import StyledLogo from './styled';
+
+const Logo = () => <StyledLogo className="heroLogo" src="../../static/ntve-logo-black.svg" alt="NVTEVISUALS logo" />
+
+export default Logo;
---
+++ Components/Logo/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const StyledLogo = styled.img`
+    padding-top: 2rem;
+    padding-left: 2rem;
+    height: 10rem;
+    @media (max-width: 720px) {
+        height: 3rem;
+    }
+`;
+
+export default StyledLogo;
