e16db2b449376bab5281c0349eaf8e3be0b661f8
Adam Mescher <admescher@gmail.com>
2019-02-06 17:06:26 -0700

    Add FullPageBackgroundImage and StyledFullPageBackgroundImage components

---
+++ Components/FullPageBackgroundImage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import StyledFullPageBackgroundImage from './styled';
+
+const FullPageBackgroundImage = ({
+    src,
+    alt
+}) => (
+        <StyledFullPageBackgroundImage src={src} alt={alt} />
+    );
+
+export default FullPageBackgroundImage;
---
+++ Components/FullPageBackgroundImage/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const StyledFullPageBackgroundImage = styled.img`
+    margin: 0;
+    padding: 0;
+    grid-area: hero;
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+`;
+
+export default StyledFullPageBackgroundImage;
