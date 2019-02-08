1639c773cfa49cbce23b37f7b35bd022846807b7
Adam Mescher <admescher@gmail.com>
2019-02-08 10:44:00 -0700

    Fix linter issues in ContactForm

--- Components/ContactForm/index.jsx
+++ Components/ContactForm/index.jsx
@@ -2,26 +2,26 @@ import React from 'react';
 import StyledContactForm from './styled';
 
 const ContactForm = () => (
-    <StyledContactForm name="contact" netlify>
-        <label>
-            name
-            <input required placeholder="name" name="name" type="text" id="name" />
-        </label>
-        <label htmlFor="email">
-            email
-            <input required placeholder="email" name="email" type="email" id="email" />
-        </label>
-        <label htmlFor="phone">
-            phone (optional)
-            <input placeholder="phone" name="phone" type="tel" id="phone" />
-        </label>
-        <label htmlFor="body">
-            message
-            <textarea required placeholder="message" name="body" id="body"></textarea>
-        </label>
-        <div data-netlify-recaptcha></div>
-        <input type="submit" value="Send Message" />
-    </StyledContactForm>
-)
+  <StyledContactForm name="contact" netlify>
+    <label>
+      name
+      <input required placeholder="name" name="name" type="text" id="name" />
+    </label>
+    <label htmlFor="email">
+      email
+      <input required placeholder="email" name="email" type="email" id="email" />
+    </label>
+    <label htmlFor="phone">
+      phone (optional)
+      <input placeholder="phone" name="phone" type="tel" id="phone" />
+    </label>
+    <label htmlFor="body">
+      message
+      <textarea required placeholder="message" name="body" id="body" />
+    </label>
+    <div data-netlify-recaptcha />
+    <input type="submit" value="Send Message" />
+  </StyledContactForm>
+);
 
 export default ContactForm;
