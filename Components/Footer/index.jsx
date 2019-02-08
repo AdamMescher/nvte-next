2c511397903390f66594a3e86f2dca4fe7b0b301
Adam Mescher <admescher@gmail.com>
2019-02-07 20:03:58 -0700

    Create Footer component

---
+++ Components/Footer/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import StyledFooter from './styled';
+
+const Footer = () => (
+    <StyledFooter>
+        <div>Footer</div>
+    </StyledFooter>
+);
+
+export default Footer;
