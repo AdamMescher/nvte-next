0e66d43b43b2e1a1b306a75ec253cff9e849a208
Adam Mescher <admescher@gmail.com>
2019-02-07 19:19:28 -0700

    Add Contact and ContactForm components

---
+++ Components/Contact/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import StyledContact from '';
+
+const Contact = () => <StyledContact />
+
+export default Contact;
---
+++ Components/ContactForm/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import StyledContactForm from './styled';
+
+const ContactForm = () => <StyledContactForm />
+
+export default ContactForm;
