1f964734065ddbee6ab4b74d9d944e92af3213d1
Adam Mescher <admescher@gmail.com>
2019-02-07 19:24:33 -0700

    Add key to images

--- Components/Gallery/index.jsx
+++ Components/Gallery/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import StyledGallery from './styled';
 import images from '../../lib/indexGalleryImages';
 
-const generateImageGallery = (images) => images.map(img => <img src={img.src} alt={img.alt} />);
+const generateImageGallery = (images) => images.map(img => <img src={img.src} alt={img.alt} key={img.title} />);
 
 const Gallery = () => (
     <StyledGallery>
