e3844b58d3f0208b20ca4dd8443db6c467ec1d8e
Adam Mescher <admescher@gmail.com>
2019-03-14 23:26:12 -0600

    Add slick-carousel cdn link to all HTML pages

--- Components/Meta/index.jsx
+++ Components/Meta/index.jsx
@@ -8,6 +8,8 @@ const Meta = () => (
     <meta name="description" content="Nate Mescher portfolio for NVTEVISUALS freelance event photography and videography" />
     <link href="https://fonts.googleapis.com/css?family=Inconsolata|Lato|Merriweather|Sansita One" rel="stylesheet" />
     <title>NVTEVISUALS</title>
+    <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
     <style>{`body { margin: 0; }`}</style>
   </Head>
 );
