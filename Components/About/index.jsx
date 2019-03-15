a59e933af6abc72856bdfeecbc43ffdebd14a590
Adam Mescher <admescher@gmail.com>
2019-03-14 23:17:37 -0600

    Update About copy

--- Components/About/index.jsx
+++ Components/About/index.jsx
@@ -5,10 +5,7 @@ const About = () => (
   <StyledAbout id="about">
     <h2>about</h2>
     <p>
-      Demonstrating agile but surprise and delight. Inform mobile-first design in order to create
-      synergy. Synchronise branding yet improve overall outcomes. Creating vertical integration and
-      then surprise and delight. Informing dark social with the possibility to re-target key
-      demographics.
+      NVTEVISUALS is a full service media coverage company. With years of experience, our goal is to effortlessly capture the highest quality content for all sizes and styles of clients. With everything available from single shooter media coverage to coordinating entire media teams to cover large scale events, our focus is to capture content for all your needs. We understand media coverage is an experience larger than the main event for which we diligently cover the overall media needs of social media, promotional material, digital marketing, enhancing the quality of your media and your brand overall. Please enjoy our gallery and hear our testimonials how we strive for excellence by seeking to improve your content and your presence!
     </p>
   </StyledAbout>
 );
