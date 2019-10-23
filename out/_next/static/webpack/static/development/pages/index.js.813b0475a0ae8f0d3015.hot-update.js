312ad7f3b4fa13f8c0be35742b3f192953ddc1fe
Adam Mescher <admescher@gmail.com>
2019-10-22 22:35:25 -0600

    Was it case sensitivity?

---
+++ out/_next/static/webpack/static/development/pages/index.js.813b0475a0ae8f0d3015.hot-update.js
@@ -0,0 +1,29 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/About/styled.js":
+/*!************************************!*\
+  !*** ./components/About/styled.js ***!
+  \************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+
+var StyledAbout = styled_components__WEBPACK_IMPORTED_MODULE_0__["default"].section.withConfig({
+  displayName: "styled__StyledAbout",
+  componentId: "sc-1lwrkna-0"
+})(["padding:6rem 0 6rem 0;display:flex;flex-direction:column;justify-content:center;h2{display:flex;justify-content:center;font-family:", ";font-size:", ";}p,li{width:60%;margin-left:20%;display:flex;justify-content:center;padding-top:2.5rem;font-family:", ";}li{margin:0;}"], function (props) {
+  return props.theme.fontStack.sansSerif;
+}, function (props) {
+  return props.theme.fontSize.h2;
+}, function (props) {
+  return props.theme.fontStack.serif;
+});
+/* harmony default export */ __webpack_exports__["default"] = (StyledAbout);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.813b0475a0ae8f0d3015.hot-update.js.map
