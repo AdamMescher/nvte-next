c8989341053233e0c584b6f63c41006dc67914bf
Adam Mescher <admescher@gmail.com>
2019-10-22 22:37:28 -0600

    Update case of imports

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,9 @@
-import Meta from '../components/Meta/index';
-import Page from '../components/Page/index';
-import Hero from '../components/Hero/index';
-import About from '../components/About/index';
-import Gallery from '../components/Gallery/index';
-import Contact from '../components/Contact/index';
+import Meta from '../Components/Meta/index';
+import Page from '../Components/Page/index';
+import Hero from '../Components/Hero/index';
+import About from '../Components/About/index';
+import Gallery from '../Components/Gallery/index';
+import Contact from '../Components/Contact/index';
 
 const Index = () => (
     <main>
