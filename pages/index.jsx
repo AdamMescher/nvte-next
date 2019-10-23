91cbadcef9294c4be4e32142be65c6a3440ec7b2
Adam Mescher <admescher@gmail.com>
2019-10-22 22:09:47 -0600

    Remove unused import

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,6 @@ import Meta from '../components/Meta/index';
 import Page from '../components/Page/index';
 import Hero from '../components/Hero/index';
 import About from '../components/About/index';
-import Testimonials from '../components/Testimonials';
 import Gallery from '../components/Gallery/index';
 import Contact from '../components/Contact/index';
 import Footer from '../components/Footer/index';
