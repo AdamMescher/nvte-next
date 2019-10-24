7792726064ca93930c3addd18c0a34088192db38
Adam Mescher <admescher@gmail.com>
2019-10-24 11:28:15 -0600

    Add frontmatter-markdown-loader to CMS and ;remove no longer needed progress bar for webpack

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,17 +4,23 @@ import Hero from '../Components/Hero/index';
 import About from '../Components/About/index';
 import Gallery from '../Components/Gallery/index';
 import Contact from '../Components/Contact/index';
+import content from '../content/home.md';
+
+let {
+  html,
+  attributes: { title, cats }
+} = content;
 
 const Index = () => (
-    <main>
-        <Meta />
-        <Page>
-            <Hero />
-            <About />
-            <Gallery />
-            <Contact />
-        </Page>
-    </main>
+  <main>
+    <Meta />
+    <Page>
+      <Hero />
+      <About />
+      <Gallery />
+      <Contact />
+    </Page>
+  </main>
 );
 
 export default Index;
