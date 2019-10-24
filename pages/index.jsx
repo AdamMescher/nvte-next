232f37dcfa681424fa397dee9ffefef756c080c8
Adam Mescher <admescher@gmail.com>
2019-10-24 11:29:47 -0600

    Remove CMS content test from home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,12 +4,6 @@ import Hero from '../Components/Hero/index';
 import About from '../Components/About/index';
 import Gallery from '../Components/Gallery/index';
 import Contact from '../Components/Contact/index';
-import content from '../content/home.md';
-
-let {
-  html,
-  attributes: { title, cats }
-} = content;
 
 const Index = () => (
   <main>
