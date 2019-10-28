7b5740d1afa6ab7cf366e935bfdb870f2476b706
Adam Mescher <admescher@gmail.com>
2019-10-28 17:36:28 -0600

    Update sample api endpoint to test axios implementation

--- pages/api/sample.js
+++ pages/api/sample.js
@@ -1,5 +1,13 @@
-export default (req, res) => {
-    res.setHeader('Content-Type', 'application/json')
-    res.statusCode = 200
-    res.end(JSON.stringify({ name: 'Nextjs' }))
+import axios from "axios";
+const getImages = async () => {
+    axios.get('http://res.cloudinary.com/nvge/image/list')
+        .then(res => console.log(res))
+        .catch(err => console.error(err))
+}
+
+export default async (request, response) => {
+    const images = await getImages();
+    response.setHeader('Content-Type', 'application/json');
+    response.statusCode = 200;
+    response.end(res.end(JSON.stringify({ images })))
 }
