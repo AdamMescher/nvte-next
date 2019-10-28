44add601933bb2a3135489ef8bae93da206e8506
Adam Mescher <admescher@gmail.com>
2019-10-28 16:51:53 -0600

    Update index.jsx page to import the new image index file

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,16 +4,7 @@ import Hero from '../Components/Hero/index';
 import About from '../Components/About/index';
 import NewGallery from '../Components/NewGallery/index';
 import Contact from '../Components/Contact/index';
-const images = [
-  {
-    alt: 'hello',
-    src: '/images/bartender.jpg'
-  },
-  {
-    alt: 'hiya',
-    src: '/images/confetti.jpg'
-  }
-];
+import images from '../lib/newGalleryImageIndex';
 
 const Index = () => (
   <main>
