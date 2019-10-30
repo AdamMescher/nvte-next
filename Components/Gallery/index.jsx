9a62bdd1c77e3326ef95e11d0a1e94e9e1f1bc40
Adam Mescher <admescher@gmail.com>
2019-10-30 17:40:58 -0600

    Remove prop spreading from Gallery component and add PropTypes

--- Components/Gallery/index.jsx
+++ Components/Gallery/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import StyledGalley from './styled';
 
-const Gallery = (props) => <StyledGalley {...props} />
+const Gallery = ({children}) => <StyledGalley children={children}/>
+
+Gallery.PropTypes = {
+    children: PropTypes.array
+}
 
 export default Gallery;
