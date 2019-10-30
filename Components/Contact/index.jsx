c66f101efb421d015895162e45b582fb3e8c3e2d
Adam Mescher <admescher@gmail.com>
2019-10-30 17:18:09 -0600

    Fix lint errors in Contact component

--- Components/Contact/index.jsx
+++ Components/Contact/index.jsx
@@ -3,10 +3,10 @@ import StyledContact from './styled';
 import ContactForm from '../ContactForm/index';
 
 const Contact = () => (
-    <StyledContact id="contact">
-        <h2>contact</h2>
-        <ContactForm />
-    </StyledContact>
+  <StyledContact id="contact">
+    <h2>contact</h2>
+    <ContactForm />
+  </StyledContact>
 );
 
 export default Contact;
