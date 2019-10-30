524366fb3399d21c8d0b6b059b9ab25badbf0fb1
Adam Mescher <admescher@gmail.com>
2019-10-30 17:53:51 -0600

    Fix lint errors for Image component and add PropTypes

--- Components/Image/index.jsx
+++ Components/Image/index.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import StyledImage from './styled';
 
-const Image = (props) => <StyledImage {...props} />
+const Image = ({
+    children,
+    onClick
+}) => <StyledImage onClick={onClick}>{children}</StyledImage>
+
+Image.propTypes = {
+    children: PropTypes.instanceOf(<img src="" alt="" />).isRequired,
+    onClick: PropTypes.func.isRequired
+  }
 
 export default Image;
