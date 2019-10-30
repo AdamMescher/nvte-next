cf96324a97afc3d163075484344f04990dc109f3
Adam Mescher <admescher@gmail.com>
2019-10-30 17:30:28 -0600

    Add prop-types dependency and add propTypes and defaultProps to FullPageBackgroundImage component to fix lint errors

--- Components/FullPageBackgroundImage/index.jsx
+++ Components/FullPageBackgroundImage/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import StyledFullPageBackgroundImage from './styled';
 
 const FullPageBackgroundImage = ({
     src,
     alt
-}) => (
-        <StyledFullPageBackgroundImage src={src} alt={alt} />
-    );
+}) => <StyledFullPageBackgroundImage src={src} alt={alt} />
+
+FullPageBackgroundImage.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string
+}
+
+FullPageBackgroundImage.defaultProps = {
+  src: '',
+  alt: ''
+}
 
 export default FullPageBackgroundImage;
