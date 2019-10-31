53d7f311cb3a400f6aabd58c37b329527c983828
Adam Mescher <admescher@gmail.com>
2019-10-30 23:39:55 -0600

    fix linting errors

--- lib/generateSocialHyperlinks.jsx
+++ lib/generateSocialHyperlinks.jsx
@@ -3,7 +3,7 @@ import socialHyperlinks from './socialHyperlinks';
 const generateSocialHyperlinks = () => (
     socialHyperlinks.map(hyperlink => (
       <li className="social-list-item" key={hyperlink.name}>
-        <a href={hyperlink.href} target="_blank">
+        <a href={hyperlink.href} target="_blank" rel="noopener noreferrer">
           <img src={hyperlink.img.src} alt={hyperlink.img.alt} />
         </a>
       </li>
